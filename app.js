808f280e195f1d96cdb25894df61d9845781547a
Lambert <lamsmallarijr@gmail.com>
2018-12-27 17:26:43 +0800

    Events and Event Handling Rolling the Dice

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ GAME RULES:
 
 What you will learn in this lecture:
 
-- How to create our fundamental game variables
-- How to generate a random number
-- How to manipulate DOM
-- How to read from the DOM
-- How to change CSS styles
+- How to setup an event handler
+- What a callback function is
+- What an anonymous function is
+- Another way to select elements by ID
+- How to change the image in an <img> element
 
 */
 
@@ -23,10 +23,25 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1. Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the result
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+});
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
-document.querySelector("#current-" + activePlayer).innerHTML =
-  "<em>" + dice + "</em>";
-
-document.querySelector(".dice").style.display = "none";
+// document.querySelector("#current-" + activePlayer).innerHTML =
+//   "<em>" + dice + "</em>";
